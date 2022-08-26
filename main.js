a0b2ffc5e6b52e66713c04ffe963d0488c016ab0
Francisco <fburgoss@emeal.nttdata.com>
2022-08-26 10:47:46 -0400

    ciclo while

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+let cont = prompt ("este contador llega solo hasta el numero 100: ingrese desde que numero desea contar");
+while (cont <=100) {
+    console.log(cont + " ");
+// imprmir por pantalla
+    document.write(cont + " ");
+    cont++;
+}
